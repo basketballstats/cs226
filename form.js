0721857feb6c5cfc8496a3ca3e86ff7d9f398eb9
basketballstats <56973668+basketballstats@users.noreply.github.com>
2019-10-28 22:35:25 -0700

    Update form.js

--- form.js
+++ form.js
@@ -1,4 +1,5 @@
- var counter = 1;
+
+        var counter = 1;
         $(document).ready(function() {
 
           var erroEle = $('.error-message'),
@@ -160,4 +161,12 @@
           }).html(appendString)
         }
 
-     
+        function validateEmail(email) {
+          var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+          return re.test(email);
+        }
+
+        function validatePhone(phone) {
+          var re = /\(?([0-9]{3})\)?([ .-]?)([0-9]{3})\2([0-9]{4})/;
+          return re.test(phone);
+        }
