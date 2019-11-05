b6f43712545ce8b9ea55924e0fa7ac30bb214b8e
basketballstats <56973668+basketballstats@users.noreply.github.com>
2019-11-05 09:51:55 -0800

    Update insert.php

--- insert.php
+++ insert.php
@@ -10,12 +10,12 @@ if($link === false){
 }
  
 // Escape user inputs for security
-$player_id = mysqli_real_escape_string($link, $_REQUEST['player_id']);
+$STATES = mysqli_real_escape_string($link, $_REQUEST['STATES']);
 $last_name = mysqli_real_escape_string($link, $_REQUEST['last_name']);
 $email = mysqli_real_escape_string($link, $_REQUEST['email']);
  
 // Attempt insert query execution
-$sql = "INSERT INTO persons (first_name, last_name, email) VALUES ('$first_name', '$last_name', '$email')";
+$sql = "INSERT INTO TEST_TABLE (STATES, last_name, email) VALUES ('$STATES', '$last_name', '$email')";
 if(mysqli_query($link, $sql)){
     echo "Records added successfully.";
 } else{
